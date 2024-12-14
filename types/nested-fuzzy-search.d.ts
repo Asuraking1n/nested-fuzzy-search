6c987a796b47bb582c87109b0722310939dc6afa
nishant <nishant@thena.ai>
2024-12-14 21:04:56 +0530

    refactor/updated d.ts file to support new output

--- types/nested-fuzzy-search.d.ts
+++ types/nested-fuzzy-search.d.ts
@@ -1,12 +1,13 @@
 declare module "nested-fuzzy-search" {
   interface SearchResult {
     path: string;
-    value: string;
+    value: string | object; // Value can be a string or an object (for arrays or objects)
     score: number;
   }
 
+  // Updated search function type definition to support both arrays and objects
   export function search(
-    data: any[],
+    data: any, // data can now be either an object or an array
     query: string,
     threshold?: number
   ): SearchResult[];
